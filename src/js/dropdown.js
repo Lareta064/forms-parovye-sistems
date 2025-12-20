bb36a274a58e81c290d676e139b89a9180f5274e
Lareta064 <lareta1@yandex.ru>
2025-12-20 13:24:58 +0400

    fix

---
+++ src/js/dropdown.js
@@ -0,0 +1,60 @@
+document.addEventListener("DOMContentLoaded", function () {
+	document.querySelector('#close-ui').addEventListener('click', ()=>{
+		document.body.classList.toggle('active')
+	});
+	
+	// DROP SELECT
+	document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
+		const dropDownBtn = dropDownWrapper.querySelector('.dropdown__button');
+		const dropDownList = dropDownWrapper.querySelector('.dropdown__list');
+		const dropDownListItems = dropDownList.querySelectorAll('.dropdown__list-item');
+		const dropDownInput = dropDownWrapper.querySelector('.dropdown__input-hidden');
+
+		// Клик по кнопке. Открыть/Закрыть select
+		dropDownBtn.addEventListener('click', function (e) {
+		
+			dropDownList.classList.toggle('dropdown__list--visible');
+			this.classList.toggle('dropdown__button--active');
+			dropDownWrapper.classList.toggle('dropdown-active');
+			
+		});
+		
+
+		// Выбор элемента списка. Запомнить выбранное значение. Закрыть дропдаун
+		dropDownListItems.forEach(function (listItem) {
+			
+			
+			listItem.addEventListener('click', function (e) {
+				e.stopPropagation();
+				const selectListItem = dropDownList.querySelector('.active');
+				if(selectListItem){
+					selectListItem.classList.remove('active');
+				}
+				this.classList.add('active');
+				dropDownBtn.innerText = this.innerText;
+				dropDownBtn.focus();
+				dropDownInput.value = this.dataset.value;
+				
+				dropDownList.classList.remove('dropdown__list--visible');
+				dropDownBtn.classList.remove('dropdown__button--active');
+				dropDownWrapper.classList.remove('dropdown-active');
+			});
+		});
+
+		// Клик снаружи дропдауна. Закрыть дропдаун
+		document.addEventListener('click', function (e) {
+			if (e.target !== dropDownBtn) {
+				dropDownBtn.classList.remove('dropdown__button--active');
+				dropDownList.classList.remove('dropdown__list--visible');
+			}
+		});
+
+		// Нажатие на Tab или Escape. Закрыть дропдаун
+		document.addEventListener('keydown', function (e) {
+			if (e.key === 'Tab' || e.key === 'Escape') {
+				dropDownBtn.classList.remove('dropdown__button--active');
+				dropDownList.classList.remove('dropdown__list--visible');
+			}
+		});
+	});
+})
