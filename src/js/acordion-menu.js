45def5656f1664054c982686b56376a26ed4d329
Lareta064 <lareta1@yandex.ru>
2026-05-02 13:08:26 +0400

    fix

--- src/js/acordion-menu.js
+++ src/js/acordion-menu.js
@@ -3,8 +3,6 @@ document.querySelectorAll(".acc__head").forEach((button) => {
     const currentAcc = button.closest(".acc");
     const parentBody = currentAcc.parentElement;
 
-    // Если нужно поведение именно аккордеона:
-    // закрываем соседей на этом же уровне
     parentBody.querySelectorAll(":scope > .acc.is-open").forEach((acc) => {
       if (acc !== currentAcc) {
         acc.classList.remove("is-open");
